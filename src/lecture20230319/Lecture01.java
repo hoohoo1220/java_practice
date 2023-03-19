2c9ed2c21640ac9affb50ff456882a2e0ba166c2
Sang Hu <bagsanghu90@gmail.com>
2023-03-19 14:10:09 +0900

    자ê¸-ëê¸ãmë£형 ì¶ 추가

---
+++ src/lecture20230319/Lecture01.java
@@ -0,0 +1,28 @@
+package lecture20230319;
+
+public class Lecture01 {
+    public static void main(String[] args) {
+
+        //기본형 primitive type
+        //정수형
+        byte number1 = 111;
+        short number2 = 11111;
+        int number3 = 1111111111;
+        long number4 = 1111111111L;
+
+        //실수형
+        float fNumber = 1111111111111111111111.0f;
+        double dNumber = 22.0;
+
+        //문자형
+        char character = 'A';
+
+        //boolean형
+        boolean flag = true;
+
+        //참조형 reference type
+        String str = "Hello, world";
+
+        //사용자 정의 타입
+    }
+}
